ab6e6863e2ab2f651084b94aa4dfa00cd8a3dd97
Danilchenko Mihail <mihaildanilchenko85@gmail.com>
2025-12-21 21:41:54 +0300

    third exersize_optimize

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,16 +1,6 @@
 const d = document.getElementById('dead')
 const l = document.getElementById('lost')
 
-const one = document.getElementById('hole1')
-const two = document.getElementById('hole2')
-const three = document.getElementById('hole3')
-const four = document.getElementById('hole4')
-const five = document.getElementById('hole5')
-const six = document.getElementById('hole6')
-const seven = document.getElementById('hole7')
-const eight = document.getElementById('hole8')
-const nine = document.getElementById('hole9')
-
 function find() {
     let z = document.querySelectorAll('[id^="hole"]')
     for (let i = 0; i < z.length; i++) {
@@ -38,12 +28,6 @@ function analiz(m) {
     } else {end()}
 }
 
-one.onclick = () => {analiz(0)}
-two.onclick = () => {analiz(1)}
-three.onclick = () => {analiz(2)}
-four.onclick = () => {analiz(3)}
-five.onclick = () => {analiz(4)}
-six.onclick = () => {analiz(5)}
-seven.onclick = () => {analiz(6)}
-eight.onclick = () => {analiz(7)}
-nine.onclick = () => {analiz(8)}
+var a = document.querySelectorAll('[id^="hole"]');
+
+for (let i = 0; i < a.length; i++) {a[i].onclick = () => {analiz(i)}}
